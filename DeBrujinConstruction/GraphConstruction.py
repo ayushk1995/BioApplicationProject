3769ca5ef6d432ae70e6ec1243d93391e8b0167f
ayushk1995 <ayushkhandelwal.ak@gmail.com>
2017-11-06 15:42:31 -0500

    Improved graph visualization Method with less dependencies

--- DeBrujinConstruction/GraphConstruction.py
+++ DeBrujinConstruction/GraphConstruction.py
@@ -1,9 +1,4 @@
-import gvmagic
-from graphviz import Digraph
-import networkx as nx
-import numpy as np
-import matplotlib.pyplot as plt
-from networkx.drawing.nx_agraph import write_dot
+import graphviz as gv
 
 
 def construct_de_bruijn_graph(str, k):
@@ -18,35 +13,7 @@ def construct_de_bruijn_graph(str, k):
 
 
 def visualize_de_bruijn_graph(nodes, edges):
-    G = nx.DiGraph()
+    t = gv.Digraph()
     for edge in edges:
-        G.add_edges_from(edge)
-    nx.write_dot(G, 'graph.dot')
-#    pos = nx.spring_layout(G)
-#    nx.draw_networkx_nodes(G, pos, label=node,  node_size=1000)
-#    nx.draw_networkx_edges(G, pos,arrows=True)
-#    nx.draw_networkx_labels(G, pos, font_size=18, font_family='sans-serif')
-#
-#     plt.show()
-
-
-
-
-
-
-
-
-#        dot = Digraph(comment='De Bruijn Grpaph')
- #       for node in nodes:
-  #          dot.node(node)
-   #     for edge in edges:
-    #        dot.edges(edge)
-     #   print(dot.source)
-
-
-# dot_str = 'digraph "DeBruijn graph" {\n'
-#    for node in nodes:
-#        dot_str += '   %s  [label="%s"];\n' % (node, node)
-#    for src, dst in edges:
-#        dot_str += '%s -> %s ;\n' % (src, dst)
-#    return dot_str + '}\n'  '''
+        t.edge(edge[0], edge[1])
+    t.render('GraphImage')
